63b3e27d2eff93f9468751abe7bc982a63f8e58c
sachinprajapati <tavarsachin@gmail.com>
2018-09-30 17:37:45 +0530

    complete file compr

--- a.sh
+++ a.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 FILE="$1"
-file_size = $(stat -c%s "$1")
-echo $file_size
 if [ -f "$1" ]
 then
     echo "FILE: $1"
-    bzip2 $1
-    du -sh $1.bz2
+	fsize="$(du -b $1 | cut -f1)"
+    bzip2 -k $1
+    gzip -k $1
+    zip -uq $1.zip $1
+    bsize="$(du -b $1.bz2 | cut -f1)"
+    gsize="$(du -b $1.gz | cut -f1)"
+    zsize="$(du -b $1.bz2 | cut -f1)"
+    echo "bzip2 \t ${bsize} \t ${fsize}"
+    echo "gzip \t ${gsize} \t ${fsize}"
+    echo "zip \t ${zsize} \t ${fsize}"
 else
     echo "file not exists"
 fi
