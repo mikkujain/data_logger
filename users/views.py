bc8cf90b84fc55fcebe890794a9ac84efda70f92
mikkujain <mayankj914@gmail.com>
2018-10-25 18:02:46 +0530

    data divide in two sensor

--- users/views.py
+++ users/views.py
@@ -43,10 +43,16 @@ class Dashboard(LoginRequiredMixin, ListView):
 		else:
 			today = datetime.today()
 			context["data_list"] = context["data_list"].filter(datetime__year=today.year, datetime__month=today.month, datetime__day=today.day).order_by("datetime")
+			dv = Devices.objects.all()
+			context["device1"] = context["data_list"].filter(port__device=dv[0])
+			context["device2"] = context["data_list"].filter(port__device=dv[1])
 		context['mobile'] = Mobile.objects.all()
-		context["max_level"] = context["data_list"].aggregate(Max('value'))["value__max"]
-		context["min_level"] = context["data_list"].aggregate(Min('value'))["value__min"]
-		context["avg_level"] = context["data_list"].aggregate(Avg('value'))["value__avg"]
+		context["device1_max"] = context["device1"].aggregate(Max('value'))["value__max"]
+		context["device2_max"] = context["device2"].aggregate(Max('value'))["value__max"]
+		context["device1_min"] = context["device1"].aggregate(Min('value'))["value__min"]
+		context["device2_min"] = context["device2"].aggregate(Min('value'))["value__min"]	
+		context["device1_avg"] = context["device1"].aggregate(Avg('value'))["value__avg"]
+		context["device2_avg"] = context["device2"].aggregate(Avg('value'))["value__avg"]
 		context["today_alert_times"] = len([i for i in context['data_list'] if i.value > i.port.alert_level])
 		#context["last_sms"] = context["sms"].latest("datetime").datetime
 		data = []
